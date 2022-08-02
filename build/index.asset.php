bfe55fac11f9578cc2f95a9f9585f4adcec296e6
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2022-08-02 18:20:53 +0000

    JS Components: Convert layout (Col, Container) components to TypeScript (#25325)
    * Rename Col component
    
    * Convert Col component to TypeScript
    
    * Rename Container component
    
    * Convert Container component to TypeScript
    
    * Rename story
    
    * changelog
    
    * Fix version
    
    * Add default values for horizontalGap and horizontalSpacing
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2784213021

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'cce6fcd44d31630d3bac');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '5b416feb56afb3595e16');
