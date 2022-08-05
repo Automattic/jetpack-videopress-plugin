ac21b5c858583c2c7cfb3cb42e4842eef4a90426
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2022-08-05 11:23:07 +0000

    JS Components: Convert AdminPage component to TypeScript (#25352)
    * Convert AdminPage component to TypeScript
    
    * Convert AdminPage stories to TypeScript
    
    * Fix story source not loading
    
    * Add changelog
    
    * Add story controls
    
    * Fix version
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2803176936

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '5b416feb56afb3595e16');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'e3152268b274f66fcc3f');
