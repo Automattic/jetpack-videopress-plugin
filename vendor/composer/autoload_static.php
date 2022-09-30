ec3f683b395d5317232bcbec6519ab3aaafe7cbf
retrofox <retrofox@users.noreply.github.com>
2022-09-30 11:45:26 +0000

    VideoPress: define initial state of the APP in the backend (#26548)
    * [not verified] add class Data to expose videos in the initial rendering
    
    * [not verified] expose initial state to the client
    
    * [not verified] pick the initial state from the global var in the client-side
    
    * changelog
    
    * count $videos to define uploadedVideoCount
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3158437512

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -168,6 +168,7 @@ class ComposerStaticInitc4802e05bbcf59fd3b6350e8d3e5482c_videopressⓥ0_2_0_alph
         'Automattic\\Jetpack\\VideoPress\\AJAX' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-videopress/src/class-ajax.php',
         'Automattic\\Jetpack\\VideoPress\\Admin_UI' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-videopress/src/class-admin-ui.php',
         'Automattic\\Jetpack\\VideoPress\\Attachment_Handler' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-videopress/src/class-attachment-handler.php',
+        'Automattic\\Jetpack\\VideoPress\\Data' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-videopress/src/class-data.php',
         'Automattic\\Jetpack\\VideoPress\\Initializer' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-videopress/src/class-initializer.php',
         'Automattic\\Jetpack\\VideoPress\\Jwt_Token_Bridge' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-videopress/src/class-jwt-token-bridge.php',
         'Automattic\\Jetpack\\VideoPress\\Module_Control' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-videopress/src/class-module-control.php',
