4a407660c42bd32090d1a65499308c5ab761072b
lhkowalski <lhkowalski@users.noreply.github.com>
2022-11-10 17:39:40 +0000

    VideoPress: create endpoints to fetch and update VideoPress settings (#27293)
    * Add endpoint to return the VideoPress privacy setting
    
    * Add changelog file
    
    * Add endpoint to update the VideoPress privacy setting
    
    * Rename API field to better explain the meaning of it
    
    * Rename API URI to inform that the context of the parameter is site-wide
    
    * Move the endpoint to the videopress/v1 namespace
    
    * Use a generic settings endpoint instead of one specific for privacy
    
    * Update the changelog file to better explain the new endpoint
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3439004412

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -183,6 +183,7 @@ class ComposerStaticInitc4802e05bbcf59fd3b6350e8d3e5482c_videopressⓥ1_0_1_alph
         'Automattic\\Jetpack\\VideoPress\\Uploader' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-videopress/src/class-uploader.php',
         'Automattic\\Jetpack\\VideoPress\\Uploader_Rest_Endpoints' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-videopress/src/class-uploader-rest-endpoints.php',
         'Automattic\\Jetpack\\VideoPress\\VideoPressToken' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-videopress/src/class-videopresstoken.php',
+        'Automattic\\Jetpack\\VideoPress\\VideoPress_Rest_Api_V1_Settings' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-videopress/src/class-videopress-rest-api-v1-settings.php',
         'Automattic\\Jetpack\\VideoPress\\VideoPress_Rest_Api_V1_Site' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-videopress/src/class-videopress-rest-api-v1-site.php',
         'Automattic\\Jetpack\\VideoPress\\VideoPress_Rest_Api_V1_Stats' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-videopress/src/class-videopress-rest-api-v1-stats.php',
         'Automattic\\Jetpack\\VideoPress\\WPCOM_REST_API_V2_Attachment_VideoPress_Data' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-videopress/src/class-wpcom-rest-api-v2-attachment-videopress-data.php',
