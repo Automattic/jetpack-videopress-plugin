d3ce6a4a5601c97c7f52eb0d7a93feffd519f4da
nunyvega <nunyvega@users.noreply.github.com>
2023-02-24 15:28:22 +0000

    Change plugin versioning from SemVer to WordPress (#29147)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4263703256

--- vendor/autoload_packages.php
+++ vendor/autoload_packages.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpc4802e05bbcf59fd3b6350e8d3e5482c_videopressⓥ1_4_1_alpha;
+namespace Automattic\Jetpack\Autoloader\jpc4802e05bbcf59fd3b6350e8d3e5482c_videopressⓥ1_5_alpha;
 
  // phpcs:ignore
 
