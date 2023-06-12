31ee936d709d03f68f7f430ca7fc35aef0368bb3
samiff <samiff@users.noreply.github.com>
2023-06-12 22:06:57 +0000

    Jetpack: backport 12.3-a.5 release (#31334)
    * Changelog edits.
    
    * Jetpack: init 12.3-a.6
    
    * mu-wpcom-plugin: init 1.4.1-alpha
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5248863424

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '2.14.3-alpha',
+      'ver' => '2.14.3',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -54,7 +54,7 @@ return array(
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.14.6-alpha',
+      'ver' => '0.14.6',
     ),
   ),
 );
