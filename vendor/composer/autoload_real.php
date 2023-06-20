bc8c86f7e87203f32bec8e9c199ef19a543dc77a
anomiex <anomiex@users.noreply.github.com>
2023-06-20 16:50:36 +0000

    Update pnpm to 8.6.2 (#31357)
    Part of the lockfile format change in 8.6.0 has been reverted, meaning
    that we need to update so people aren't flip-flopping the setting based
    on the version of pnpm they happen to have.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5325073318

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -32,25 +32,18 @@ class ComposerAutoloaderInitc4802e05bbcf59fd3b6350e8d3e5482c_videopressⓥ1_6_al
         $loader->setClassMapAuthoritative(true);
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInitc4802e05bbcf59fd3b6350e8d3e5482c_videopressⓥ1_6_alpha::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequirec4802e05bbcf59fd3b6350e8d3e5482c_videopressⓥ1_6_alpha($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitc4802e05bbcf59fd3b6350e8d3e5482c_videopressⓥ1_6_alpha::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequirec4802e05bbcf59fd3b6350e8d3e5482c_videopressⓥ1_6_alpha($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
