cd9c0712d4360f5a827c064f07046ef4e5d0ad96
anomiex <anomiex@users.noreply.github.com>
2024-02-09 17:51:56 +0000

    Update monorepo to Composer 2.7.1 (#35574)
    Apparently 2.7.0 fixed a security bug, so people want this updated
    sooner rather than later.
    
    Fortunately there's no API version change this time around so no lock
    files need updating.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7847615616

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'automattic/jetpack-videopress-plugin',
         'pretty_version' => 'dev-trunk',
         'version' => 'dev-trunk',
-        'reference' => NULL,
+        'reference' => null,
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -220,7 +220,7 @@
         'automattic/jetpack-videopress-plugin' => array(
             'pretty_version' => 'dev-trunk',
             'version' => 'dev-trunk',
-            'reference' => NULL,
+            'reference' => null,
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
