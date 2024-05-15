b645abb148c4e336686bfaef0fb5d993a945dfca
jeherve <jeherve@users.noreply.github.com>
2024-05-15 07:16:53 +0000

    VideoPress: Fix localization of block text (#37360)
    * VideoPress: Fix localization of block text
    
    * changelog
    
    * Fix versions
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9091391602
    
    Upstream-Ref: Automattic/jetpack@9393263c72c87a4143c6efb68de6f8547ca4df97

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -62,7 +62,7 @@ return array(
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.23.22-alpha1715706972',
+      'ver' => '0.23.22-alpha1715756611',
     ),
   ),
 );
