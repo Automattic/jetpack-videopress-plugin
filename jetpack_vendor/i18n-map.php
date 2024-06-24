81fc8716620ce1437dfbe9bfc9a3473936bc429d
zinigor <zinigor@users.noreply.github.com>
2024-06-24 20:09:35 +0000

    Backport mu-wpcom-plugin 2.3.0, jetpack 13.6-a.7, wpcomsh 3.24.0 Changes (#38017)
    * Changelog and readme.txt edits.
    
    * Init new cycle
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9651561792
    
    Upstream-Ref: Automattic/jetpack@db8462d39625b9ffd95d1a74aa0f6408a78d5a79

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -22,7 +22,7 @@ return array(
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
-      'ver' => '2.0.4-alpha1718985515',
+      'ver' => '2.0.4',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.25.3-alpha1719070359',
+      'ver' => '4.25.3',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -58,7 +58,7 @@ return array(
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.23.26-alpha1718966443',
+      'ver' => '0.23.26',
     ),
   ),
 );
