429e975fe404f79ae58d231db4eb33fac648ece1
simison <simison@users.noreply.github.com>
2024-09-20 10:24:21 +0000

    VideoPress: add unique ID attributes to videopress block and shortcode (#39460)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10957356495
    
    Upstream-Ref: Automattic/jetpack@7b91eb58916f3e63aece2d7b2fe253fc93f6e6ea

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.24.8',
+      'ver' => '0.25.0-alpha1726826973',
     ),
   ),
 );
