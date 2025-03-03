b881dfcfe243b5696cac8833d68eb89019242c1c
anomiex <anomiex@users.noreply.github.com>
2025-03-03 18:37:41 +0000

    phpunit: Rename package test classes for PHPUnit 11 compat (pt 2) (#42128)
    PHPUnit 10+ requires that test class names match the filename, PSR-4
    style. This PR renames the test classes and files in packages to end in
    `Test` or `_Test`, depending on whether the class names are following
    WordPress's underscore-naming convention or are using CamelCase, and
    removing redundant `WP_Test_` or `Test_` prefixes.
    
    This also adds our standard Jetpack-Tests ruleset to
    tests/.phpcs.dir.xml in the packages, and removes some redundant inline
    phpcs:ignore or phpcs:disable directives.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13637625724
    
    Upstream-Ref: Automattic/jetpack@f1afaaa394d6d749df425bfe7d3e0a82cce78972

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.3',
+      'ver' => '0.4.4-alpha1741025906',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
@@ -58,19 +58,19 @@ return array(
     ),
     'jetpack-protect-models' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-models',
-      'ver' => '0.5.1',
+      'ver' => '0.5.2-alpha1741025906',
     ),
     'jetpack-protect-status' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-status',
-      'ver' => '0.5.3-alpha1740760540',
+      'ver' => '0.5.3-alpha1741025906',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.8.2',
+      'ver' => '4.8.3-alpha1741025906',
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.27.0-alpha1741019398',
+      'ver' => '0.27.0-alpha1741025906',
     ),
   ),
 );
