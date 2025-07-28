3c9e6960d5fca46852ee87464122b8509e1a9840
tbradsha <tbradsha@users.noreply.github.com>
2025-07-28 21:15:53 +0000

    Changelog and readme.txt edits. (#44507)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16580196806
    
    Upstream-Ref: Automattic/jetpack@21f3e21f44fa6484ac649abde838b908b5e08cf5

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.16.1-alpha1753719965',
+      'ver' => '6.16.1',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.20.2-alpha1753672780',
+      'ver' => '5.20.2',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -66,11 +66,11 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.16.0-alpha1753710227',
+      'ver' => '4.16.0',
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.30.6-alpha1753277640',
+      'ver' => '0.30.6',
     ),
   ),
 );
