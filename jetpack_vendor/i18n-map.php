d771e7ecd954a96cd80a9884144078dc4593c052
tbradsha <tbradsha@users.noreply.github.com>
2025-08-13 13:43:38 +0000

    Prevent PHP fatals when directly accessing files, round 3 (#44764)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16938673408
    
    Upstream-Ref: Automattic/jetpack@bda2a8cfb01610bbc49d77fe7072c10c0bd079fd

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.3-alpha1754976850',
+      'ver' => '4.3.3-alpha1755091645',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.22.2-alpha1754976850',
+      'ver' => '5.22.2-alpha1755091645',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -54,7 +54,7 @@ return array(
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.5.6',
+      'ver' => '0.5.7-alpha1755091645',
     ),
     'jetpack-protect-models' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-models',
@@ -66,7 +66,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.18.1',
+      'ver' => '4.18.2-alpha1755091645',
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
