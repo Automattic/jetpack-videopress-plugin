4ac057b40a9df204a5a32b5be0410e58cf9a61c1
gmjuhasz <gmjuhasz@users.noreply.github.com>
2025-10-07 13:44:03 +0000

    Backport jetpack 15.1-beta Changes (#45382)
    * Changelog and readme.txt edits.
    
    * Amend readme.txt
    
    * Apply suggestions from code review
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/18314235220
    
    Upstream-Ref: Automattic/jetpack@50be87a3edddb5367ce85a2ca70146769f55c319

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.3.12-alpha1759349161',
+      'ver' => '0.3.12',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.7-alpha1759384048',
+      'ver' => '4.3.7',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.27.3-alpha1759351769',
+      'ver' => '5.27.3',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.32.9-alpha1759351769',
+      'ver' => '0.32.9',
     ),
   ),
 );
