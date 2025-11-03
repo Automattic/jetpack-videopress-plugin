3bacf5396f89ade905b37ff4f877893505d3888e
gmjuhasz <gmjuhasz@users.noreply.github.com>
2025-11-03 15:03:03 +0000

    Backport jetpack 15.2-beta Changes (#45745)
    * Changelog and readme.txt edits.
    
    * Amend readme.txt
    
    * Apply suggestions from code review
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/19038501802
    
    Upstream-Ref: Automattic/jetpack@f31f2fc45d0159863b4203a021e451b91ae873c6

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.19.2',
+      'ver' => '6.19.3',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.28.0-alpha1761736151',
+      'ver' => '5.28.0',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -66,11 +66,11 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.22.3-alpha1761937079',
+      'ver' => '4.22.3',
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.32.12-alpha1761937079',
+      'ver' => '0.32.12',
     ),
   ),
 );
