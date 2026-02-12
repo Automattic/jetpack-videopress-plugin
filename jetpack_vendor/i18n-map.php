8061b855dd483c711d5415587faf165c0b84ea86
anomiex <anomiex@users.noreply.github.com>
2026-02-12 20:59:24 +0000

    Update babel monorepo (#47099)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/21963441270
    
    Upstream-Ref: Automattic/jetpack@1b7ee4b74215f46da666ae383c40c8f2fe742e63

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '7.1.0-alpha1770741545',
+      'ver' => '7.1.0-alpha1770928754',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.11-alpha1770924502',
+      'ver' => '0.4.11-alpha1770928754',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.31.3-alpha1770906822',
+      'ver' => '5.31.3-alpha1770928754',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.35.0-alpha1770750400',
+      'ver' => '0.35.0-alpha1770928754',
     ),
   ),
 );
