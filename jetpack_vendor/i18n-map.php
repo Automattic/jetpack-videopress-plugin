da439caa8e49038932a6608cb96b9b3720ed2f97
gmjuhasz <gmjuhasz@users.noreply.github.com>
2026-03-02 13:42:03 +0000

    Backport jetpack 15.6-beta changes (#47405)
    * Changelog and readme.txt edits.
    
    * Amend readme.txt
    
    * Update stable tag in readme.txt
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/22577875067
    
    Upstream-Ref: Automattic/jetpack@cdce4a0b57d85b5cf0b3d07bbfe3496866c53cc6

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -26,7 +26,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.0.1',
+      'ver' => '8.0.2',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.31.8-alpha1772444523',
+      'ver' => '5.31.8',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -66,11 +66,11 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.31.0-alpha1772221134',
+      'ver' => '4.31.0',
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.35.2',
+      'ver' => '0.35.3',
     ),
   ),
 );
