3a3f654ca21936845e12481cdfa15ffb0e9373c8
robertsreberski <robertsreberski@users.noreply.github.com>
2026-06-25 12:08:50 +0000

    Backport jetpack 16.0-a.5 changes (#49962)
    * Baseline changelogger run
    
    * Refined changelog
    
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28167992302
    
    Upstream-Ref: Automattic/jetpack@02543fe2c360c56e2b492608d75f0df73e62d63c

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.7.4-alpha1782313868',
+      'ver' => '8.7.4',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.40.2-alpha1782312478',
+      'ver' => '5.40.2',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -70,15 +70,15 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.44.1-alpha1782309804',
+      'ver' => '4.44.1',
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.38.1-alpha1782312478',
+      'ver' => '0.38.1',
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
-      'ver' => '0.1.17-alpha1782319868',
+      'ver' => '0.1.17',
     ),
   ),
 );
