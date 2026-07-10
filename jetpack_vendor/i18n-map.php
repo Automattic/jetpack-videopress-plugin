b202b8517f2bed85bc3dab739ae7639cc976fecf
anomiex <anomiex@users.noreply.github.com>
2026-07-10 18:45:34 +0000

    Clean up JSX-in-`.js` (#50418)
    Babel 7's `@babel/preset-typescript` was smart enough to know that
    `.tsx` files contain JSX syntax while `.ts` files don't. For Babel 8,
    they changed it to ignore the extension in favor of looking at whether
    `@babel/preset-react` is loaded, and then it chokes on certain
    TypeScript syntax that can be confused with JSX. The recommended fix is
    to only load apply `@babel/preset-react` for `.tsx` files, not `.ts`.
    
    In doing that, I decided to clean up our long-standing situation where
    `.js` files sometimes contain JSX syntax. Mostly that meant identifying
    and renaming 770 files and cleaning up references to them, which I
    handed off to Claude.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29114730006
    
    Upstream-Ref: Automattic/jetpack@b531b5e11395cf32445d504ad11b9af72c378a3f

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -54,7 +54,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.40.6',
+      'ver' => '5.40.7-alpha1783708168',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -78,7 +78,7 @@ return array(
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.41.0-alpha1783625838',
+      'ver' => '0.41.0-alpha1783708168',
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
