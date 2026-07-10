e1c103d54fefd7499adec87656f6b7df6325e70d
obenland <obenland@users.noreply.github.com>
2026-07-10 23:52:06 +0000

    VideoPress: reuse attachment title, description & caption when promoting a Media Library video (#50427)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29130761737
    
    Upstream-Ref: Automattic/jetpack@26c8130394322040b945bc02fed6a0241d2c1ba9

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -78,7 +78,7 @@ return array(
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.41.0-alpha1783710258',
+      'ver' => '0.41.0-alpha1783726618',
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
