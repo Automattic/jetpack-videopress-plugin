ba482db0cdef47837b5177769149bacf39decc2a
anomiex <anomiex@users.noreply.github.com>
2026-07-22 17:25:58 +0000

    Update dependency postcss to v8.5.21 (#50753)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29941057274
    
    Upstream-Ref: Automattic/jetpack@8b1c5c274f79a5d3b9f522356a39c73cf011651d

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -54,7 +54,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.40.10-alpha1784734664',
+      'ver' => '5.40.10-alpha1784740091',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -78,7 +78,7 @@ return array(
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.43.0-alpha1784734664',
+      'ver' => '0.43.0-alpha1784740091',
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
