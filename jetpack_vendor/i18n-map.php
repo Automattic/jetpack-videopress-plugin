9e2c339d6944f09fe8310f1b7ea46b78a4157650
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2026-08-04 05:47:03 +0000

    Update dependency postcss to v8.5.23 [SECURITY] (#51008)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/30880385614
    
    Upstream-Ref: Automattic/jetpack@f3849f5ba16e764483cc315b106b0801c3c7f761

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -54,7 +54,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.41.0',
+      'ver' => '5.41.1-alpha1785820826',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -78,7 +78,7 @@ return array(
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.44.0-alpha1785514440',
+      'ver' => '0.44.0-alpha1785820826',
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
