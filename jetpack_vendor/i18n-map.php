dc85c9b32b5492df1bd1e0c99238d61dbc2ee033
anomiex <anomiex@users.noreply.github.com>
2026-08-25 19:58:56 +0000

    Phan: Update config for PHP 7.4 (#51571)
    This reverts the temporary ignore of
    `PhanPluginDuplicateExpressionAssignmentOperation` added in #51515, and
    fixes all the places where it was reporting.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32890661658
    
    Upstream-Ref: Automattic/jetpack@c858dc6620682e61f9e3e30da09dd219c432f146

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '5.0.0-alpha1787677819',
+      'ver' => '5.0.0-alpha1787686674',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
