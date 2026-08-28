81e6660ee692a97adbf7ec394b3da3aaae54fc1f
anomiex <anomiex@users.noreply.github.com>
2026-08-28 18:57:29 +0000

    phan: Update custom stubs (#51694)
    Then fix Phan issues from WP 7.1 stub update:
    * `wp_send_json*` functions are now marked as never returning. Remove
      unreachable code flagged by Phan, and add some `@return never` of our
      own for good measure.
    * `WP_Screen->parent_file` changed from `string|null` to `?string`,
      which makes Phan complain when passed to a non-nullable parameter of a
      function. Cast it to string to preserve existing behavior.
    * `WP_Network->blog_id` changed the declared type from `int` (despite
      containing numeric strings, apparently) to `string`. Add a cast when
      passing to a function taking ints.
    
    ---------
    
    Co-authored-by: matticbot <matticbot@users.noreply.github.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33200129988
    
    Upstream-Ref: Automattic/jetpack@b488de115c2808421c0a9f98382d43aee0623a99

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.0.1-alpha1787779161',
+      'ver' => '9.0.1-alpha1787942239',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -78,11 +78,11 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '5.0.1-alpha1787777208',
+      'ver' => '5.0.1-alpha1787942239',
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.49.0-alpha1787938907',
+      'ver' => '0.49.0-alpha1787942239',
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
