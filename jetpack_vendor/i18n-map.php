eee76b93ed6fb9d73fb1df045692e3abd02f4731
taipeicoder <taipeicoder@users.noreply.github.com>
2026-09-10 04:26:19 +0000

    VideoPress: add a "Learn more" support link to the admin page (#52111)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34435857949
    
    Upstream-Ref: Automattic/jetpack@2ccdb4939fb45873b7ebb6d3ea2b5e62accf91eb

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -78,7 +78,7 @@ return array(
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.50.1-alpha1788902387',
+      'ver' => '0.50.1-alpha1789013170',
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
