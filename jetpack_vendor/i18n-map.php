13e8a9e4993865f2c993bbc327a910fa9009e1de
vianasw <vianasw@users.noreply.github.com>
2026-09-11 16:54:25 +0000

    VideoPress: Cover colons in chapter titles (#51189)
    * AI: implement task VIDP-270
    
    Co-authored-by: matticbot <matticbot@users.noreply.github.com>
    
    * VideoPress: Cover colon chapter titles without changing the parser
    
    ---------
    
    Co-authored-by: heydemoura <heyde.moura@automattic.com>
    Co-authored-by: matticbot <matticbot@users.noreply.github.com>
    Co-authored-by: William Viana <vianasw@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34622679537
    
    Upstream-Ref: Automattic/jetpack@127ddadc04c58aee60e88fcaa22054798b203d64

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -78,7 +78,7 @@ return array(
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.50.2-alpha1789138304',
+      'ver' => '0.50.2-alpha1789144406',
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
