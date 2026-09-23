ea576b7b74cbc6761af3d6a0173e65fa159c2549
retrofox <retrofox@users.noreply.github.com>
2026-09-23 17:11:29 +0000

    Premium Analytics: say where each widget module's translations live (#52634)
    * add the catalog location to widget module records
    textdomain and i18n_manifest per type; the client loads a plugin's widget catalogs from them
    
    * apply the review follow-ups from #52568
    
    require default sections in the registry test, drop the version-shape test, fix the availability header
    
    * mock the widget module resolver in the stage test
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35891390619
    
    Upstream-Ref: Automattic/jetpack@a756062f975c2a85945e2df2e37759b4e4323c1e

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -1103,11 +1103,11 @@ return array(
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-videopress/src/class-xmlrpc.php'
 	),
 	'Automattic\\Jetpack\\WP_Build_Polyfills\\WP_Build_Admin_Frame' => array(
-		'version' => '0.5.8.0',
+		'version' => '0.6.0.0-alpha1790182149',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-wp-build-polyfills/src/class-wp-build-admin-frame.php'
 	),
 	'Automattic\\Jetpack\\WP_Build_Polyfills\\WP_Build_Polyfills' => array(
-		'version' => '0.5.8.0',
+		'version' => '0.6.0.0-alpha1790182149',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-wp-build-polyfills/src/class-wp-build-polyfills.php'
 	),
 	'Container' => array(
