da8b65770b5feac298d5d04f444ed2d65eb69b63
vianasw <vianasw@users.noreply.github.com>
2026-09-24 15:14:47 +0000

    VideoPress: handle poster generation failures (#52759)
    * VideoPress: handle poster generation failures
    
    * VideoPress: preserve poster error translations during minification
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/36016172520
    
    Upstream-Ref: Automattic/jetpack@03945d7df979cb4ac3a2dbe731b6fb794a411663

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -82,7 +82,7 @@ return array(
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.52.0',
+      'ver' => '0.52.1-alpha1790261643',
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
